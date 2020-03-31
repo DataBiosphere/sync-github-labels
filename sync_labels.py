6fb81a72e4fe1fc0098545750d1d31de55aac4ea
Charles Reid <chmreid@ucsc.edu>
2020-03-31 13:05:19 -0700

    add rate limit exception handling

--- sync_labels.py
+++ sync_labels.py
@@ -2,7 +2,7 @@
 import sys
 import os
 import argparse
-from github import Github, GithubException
+from github import Github, GithubException, RateLimitExceededException
 
 """
 Sync Github Labels
@@ -26,10 +26,14 @@ def sync_labels(src_repo, dest_repo, dry_run, force, delete):
     g = Github(get_access_token())
     try:
         s = g.get_repo(src_repo)
+    except RateLimitExceededException:
+        raise Exception(f"API rate limit hit! Try again later.")
     except GithubException:
         raise Exception(f"Source repo is not a repo: {src_repo}")
     try:
         d = g.get_repo(dest_repo)
+    except RateLimitExceededException:
+        raise Exception(f"API rate limit hit! Try again later.")
     except GithubException:
         raise Exception(f"Destination repo is not a repo: {dest_repo}")
 
@@ -38,18 +42,18 @@ def sync_labels(src_repo, dest_repo, dry_run, force, delete):
     src_label_names = set([j.name for j in src_labels])
     dest_labels = list(d.get_labels())
     dest_label_names = set([j.name for j in dest_labels])
-    dest_add = src_label_names - dest_label_names
-    dest_del = dest_label_names - src_label_names
+    dest_add = sorted(list(src_label_names - dest_label_names))
+    dest_del = sorted(list(dest_label_names - src_label_names))
 
     # Show summary of what actions will be taken, get confirmation
     print(f"The following labels will be created in {dest_repo}:")
     for lab in sorted(list(dest_add)):
-        print(f" - {lab}")
+        print(f" [+] {lab}")
 
     if delete:
         print(f"The following labels will be deleted in {dest_repo}:")
         for lab in sorted(list(dest_del)):
-            print(f" - {lab}")
+            print(f" [-] {lab}")
 
     if not force:
         get_user_confirmation()
@@ -64,14 +68,16 @@ def sync_labels(src_repo, dest_repo, dry_run, force, delete):
         color = label.color
         description = label.description
         try:
-            d.create_label(name, color, description)
+            import pdb; pdb.set_trace()
+            d.create_label(name, color)
+            #d.create_label(name, color, description)
             print(f"Success!")
         except GithubException:
             raise Exception(f"Error creating label {name} in destination repo {dest_repo}")
 
     # Delete labels
     for name in dest_del:
-        print(f"Removing label {name} from destination repo {dest_repo}...")
+        print(f"Deleting label {name} from destination repo {dest_repo}...")
         if dry_run:
             print(f"Dry run successful!")
             continue
@@ -112,7 +118,7 @@ def main(sysargs=sys.argv[1:]):
     # Parse the arguments
     args = parser.parse_args(sysargs)
 
-    print(f"Syncing labels from {args.src} to {args.src}")
+    print(f"Syncing labels from {args.src} to {args.dest}")
     sync_labels(args.src, args.dest, args.dry_run, args.force, args.delete)
 
 
